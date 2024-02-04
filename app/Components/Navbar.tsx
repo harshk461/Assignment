5ae0365737047a5217c93bdb61f1bbea424c0d1d
Harsh <hk2152573@gmail.com>
2024-02-04 12:14:59 +0530

    first commit

---
+++ app/Components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div className='w-full flex px-6 py-3 justify-between items-center'>
+
+        </div>
+    )
+}
